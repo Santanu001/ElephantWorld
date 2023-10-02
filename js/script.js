93ebda1eab715fed2a5e354e296639b319e80528
Santanu <hsantanu836@gmail.com>
2023-10-03 02:50:59 +0530

    code changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-const header = document.querySelector(".header");
-const mob_NavBtn = document.querySelector(".mobile-navbar-btn");
+// const header = document.querySelector(".header");
+// const mob_NavBtn = document.querySelector(".mobile-navbar-btn");
 
-mob_NavBtn.addEventListener("click", ()=>toggleNavbar());
+// mob_NavBtn.addEventListener("click", ()=>toggleNavbar());
 
-const toggleNavbar = () => {
+// const toggleNavbar = () => {
+//     header.classList.toggle("active");
+// }
+
+var header = document.querySelector(".header");
+var mob_NavBtn = document.querySelector(".mobile-navbar-btn");
+
+mob_NavBtn.addEventListener("click", toggleNavbar);
+
+function toggleNavbar()  {
     header.classList.toggle("active");
 }
